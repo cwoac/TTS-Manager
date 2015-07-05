58d120cda0a7550412d02993e4b1522e97943bc4
Oliver Matthews <ojm@codersoffortune.net>
2015-07-05 17:56:19 +0100

    abstract out the file lister.

--- tts_gui.py
+++ tts_gui.py
@@ -4,48 +4,10 @@ import tkinter.ttk as ttk
 import tkinter.scrolledtext as ScrolledText
 import os.path
 
-class TTS_GUI:
-  def list_command(self):
-    data=None
-    if self.isSaves.get():
-      data=tts.describe_save_files()
-    else:
-      data=tts.describe_workshop_files()
-    self.file_list.delete(0,Tk.END)
-    self.file_store={}
-    i=0
-    for (name,number) in data:
-      self.file_list.insert(Tk.END,"%s (%s)" % (name,number))
-      self.file_store[i]=number
-      i+=1
-    self.file_list_current = None
-    self.poll_file_list()
-
-  def poll_file_list(self):
-    now = self.file_list.curselection()
-    if now != self.file_list_current:
-      self.file_list_has_changed(now)
-      self.file_list_current=now
-    self.root.after(250,self.poll_file_list)
-
-  def file_list_has_changed(self,now):
-    if not now:
-      return
-    ident=self.file_store[now[0]]
-    data=None
-    if self.isSaves.get():
-      data=tts.load_save_file(ident)
-    else:
-      data=tts.load_workshop_file(ident)
-    # TODO: error handling
-    save=tts.Save(data)
-    self.details_list.config(state=Tk.NORMAL)
-    self.details_list.delete(1.0,Tk.END)
-    self.details_list.insert(Tk.END,save)
-    self.details_list.config(state=Tk.DISABLED)
-
-  def populate_list(self,frame):
-    srcFrame=ttk.Frame(frame)
+class SaveBrowser():
+  def __init__(self,master,poll_command):
+    self.master=master
+    srcFrame=ttk.Frame(master)
     srcFrame.pack()
     ttk.Label(srcFrame,text="Select list source:").pack()
     self.isSaves=Tk.BooleanVar()
@@ -59,19 +21,72 @@ class TTS_GUI:
                    variable=self.isSaves,
                    value=True,
                    command=self.list_command).pack(side=Tk.LEFT)
-    listFrame=ttk.Frame(frame)
-    listFrame.pack(expand=1,fill=Tk.BOTH)
-    ttk.Label(listFrame,text="Files found:").pack()
-    foundBar=ttk.Scrollbar(listFrame,orient=Tk.VERTICAL)
-    self.file_list=Tk.Listbox(listFrame,yscrollcommand=foundBar.set)
+    scrollFrame=ttk.Frame(master)
+    scrollFrame.pack(expand=1,fill=Tk.BOTH)
+    ttk.Label(scrollFrame,text="Files found:").pack()
+    foundBar=ttk.Scrollbar(scrollFrame,orient=Tk.VERTICAL)
+    self.file_list=Tk.Listbox(scrollFrame,yscrollcommand=foundBar.set)
     foundBar.config(command=self.file_list.yview)
     foundBar.pack(side=Tk.RIGHT,fill=Tk.Y)
     self.file_list.pack(side=Tk.LEFT,fill=Tk.BOTH,expand=1)
+    self.poll_command=poll_command
+    self.file_list_current = None
+    self.poll_file_list()
+
+  def list_command(self):
+    """ Populates the list box"""
+    data=None
+    if self.isSaves.get():
+      data=tts.describe_save_files()
+    else:
+      data=tts.describe_workshop_files()
+    self.file_list.delete(0,Tk.END)
+    self.file_store={}
+    i=0
+    for (name,number) in data:
+      self.file_list.insert(Tk.END,"%s (%s)" % (name,number))
+      self.file_store[i]=number
+      i+=1
+
+
+  def poll_file_list(self):
+    now = self.file_list.curselection()
+    if now != self.file_list_current:
+      self.file_list_has_changed(now)
+      self.file_list_current=now
+    self.master.after(250,self.poll_file_list)
+
+  def file_list_has_changed(self,now):
+    if not now:
+      return
+    ident=self.file_store[now[0]]
+    data=None
+    if self.isSaves.get():
+      data=tts.load_save_file(ident)
+    else:
+      data=tts.load_workshop_file(ident)
+    # TODO: error handling
+    save=tts.Save(data)
+    self.poll_command(save)
+
+
+
+class TTS_GUI:
+  def update_details(self,savedata):
+    self.details_list.config(state=Tk.NORMAL)
+    self.details_list.delete(1.0,Tk.END)
+    self.details_list.insert(Tk.END,savedata)
+    self.details_list.config(state=Tk.DISABLED)
+
+  def populate_list(self,frame):
+    self.sb=SaveBrowser(frame,self.update_details)
+    self.file_list_current = None
     ttk.Label(frame,text="Details:").pack()
     self.details_list=ScrolledText.ScrolledText(master=frame,height=5)
     self.details_list.config(state=Tk.DISABLED)
     self.details_list.pack(fill=Tk.BOTH, expand=Tk.Y)
-    self.list_command()
+    self.sb.list_command()
+
 
   def __init__(self,root):
     self.root=root
