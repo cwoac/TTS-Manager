b4ca4657b4a78edeb4ddacdcbebae8704468bc98
Oliver Matthews <ojm@codersoffortune.net>
2015-07-09 09:19:54 +0100

    strip test modules from compiled binaries

--- setup.py
+++ setup.py
@@ -3,14 +3,23 @@ import py2exe,sys,os
 
 sys.argv.append("py2exe")
 
+
+
 setup(
-  options = {'py2exe': {'optimize':2,'bundle_files':1,'compressed':True}},
+  options = {'py2exe': {'optimize':2,
+                        'bundle_files':1,
+                        'compressed':True,
+                        'excludes':['doctest','pdb','unittest','inspect','difflib']}},
   zipfile=None,
+
   console=['tts_cli.py']
 )
 
 setup(
-  options = {'py2exe': {'optimize':2,'bundle_files':2,'compressed':True}},
+  options = {'py2exe': {'optimize':2,
+                        'bundle_files':2,
+                        'compressed':True,
+                        'excludes':['doctest','pdb','unittest','inspect','difflib']}},
   zipfile=None,
   windows=[{'script':'tts_gui.py'}]
 )
