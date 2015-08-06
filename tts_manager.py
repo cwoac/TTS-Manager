6088aaea94c726a7ec00a9f88c2924ea55644361
Oliver Matthews <ojm@codersoffortune.net>
2015-08-06 11:42:13 +0100

    add buttons. Implement basic download functionality.

--- tts_manager.py
+++ tts_manager.py
@@ -27,6 +27,11 @@ class TTS_MANAGER:
     self.filesystem=self.preferences.get_filesystem()
     self.populate_manage_frame(root)
 
+  def reload_filesystem(self):
+    self.filesystem=self.preferences.get_filesystem()
+    self.file_list_current=None
+    self.file_list_box.selection_clear(0,Tk.END)
+
   def populate_manage_frame(self,frame):
     ttk.Label(frame,text="Select list source:").grid(row=0,sticky=Tk.W)
     self.save_type=Tk.IntVar()
@@ -56,12 +61,71 @@ class TTS_MANAGER:
     self.file_list_box.pack(side=Tk.LEFT,fill=Tk.BOTH,expand=Tk.Y)
     self.file_list_box.config(state=Tk.DISABLED)
 
-    for x in range(4):
-        Tk.Grid.columnconfigure(frame,x,weight=1)
-    for y in range(2):
-        Tk.Grid.rowconfigure(frame,y,weight=1)
+    self.downloadButton=ttk.Button(frame,
+                                   text="Download",
+                                   command=self.download,
+                                   state=Tk.DISABLED)
+    self.downloadButton.grid(row=2,column=0)
+    self.exportButton=ttk.Button(frame,
+                                 text="Export",
+                                 command=self.export,
+                                 state=Tk.DISABLED)
+    self.exportButton.grid(row=2,column=1)
 
+    ttk.Button(frame,text="Preferences",command=self.showPreferences).grid(row=3)
+
+    # Enable resizing
+    for x in range(4):
+      Tk.Grid.columnconfigure(frame,x,weight=1)
+    for y in range(3):
+      Tk.Grid.rowconfigure(frame,y,weight=1)
+
+    self.file_list_current=None
     self.list_command()
+    self.poll_file_list()
+
+  def download(self):
+    if not self.save:
+      tts.logger().warn("Internal error: self.save NULL when attempting to download")
+      messagebox.showinfo("TTS Manager","Download failed (see log).")
+      return
+    if self.save.download():
+      messagebox.showinfo("TTS Manager","Download done.")
+    else:
+      messagebox.showinfo("TTS Manager","Download failed (see log).")
+    self.file_list_has_changed(self.file_list_current)
+
+  def export(self):
+    pass
+
+  def poll_file_list(self):
+    now = self.file_list_box.curselection()
+    if now != self.file_list_current:
+      self.file_list_has_changed(now)
+      self.file_list_current=now
+    self.root.after(250,self.poll_file_list)
+
+  def file_list_has_changed(self,now):
+    if not now:
+      return
+    ident=self.file_store[now[0]]
+    filename=self.filesystem.get_json_filename_for_type(ident,self.save_type.get())
+    data=tts.load_json_file(filename)
+    # TODO: error handling
+    self.save=tts.Save(savedata=data,
+                       ident=ident,
+                       filename=filename,
+                       save_type=tts.SaveType(self.save_type.get()),
+                       filesystem=self.filesystem)
+    if self.save.isInstalled:
+      self.downloadButton.configure(text="All files Downloaded",
+                                    state=Tk.DISABLED)
+      self.exportButton.configure(text="Export",state=Tk.NORMAL)
+    else:
+      self.downloadButton.configure(text="Download missing files",
+                                    state=Tk.NORMAL)
+      self.exportButton.configure(text="Download and Export",
+                                  state=Tk.NORMAL)
 
   def list_command(self):
     """ Populates the list box"""
